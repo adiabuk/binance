aad5fe8ed04ca76967927dd6cdf22f531557ee5b
Amro Diab <adiab@hotmail.co.uk>
2019-07-14 23:46:33 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='binance',
-      version='0.3',
+      version='0.4',
       py_modules=['binance'],
       description='Binance API wrapper',
       url='https://github.com/toshima/binance',
