9149d4eed05868ead30be276146adc7177fe862c
Amro Diab <adiab@hotmail.co.uk>
2021-01-04 12:39:04 +0000

    add unit tests

---
+++ tests/test_lint.py
@@ -0,0 +1,26 @@
+
+"""Test lint of python files"""
+
+import unittest
+from pathlib import Path
+from pylint.lint import Run
+
+class TestLint(unittest.TestCase):
+    """Test Lint Unittest"""
+
+    def test_lint(self):
+        """
+        test lint
+        """
+        for filename in Path('binance').rglob('*.py'):
+            file = open(str(filename))
+            count = len(file.readlines())
+            file.close()
+            if 'test' not in str(filename) and count > 5:
+                results = Run([str(filename)], do_exit=False)
+                try:
+                    print(filename)
+                    score = results.linter.stats['global_note']
+                    self.assertGreaterEqual(score, 9.0)
+                except KeyError:
+                    pass
