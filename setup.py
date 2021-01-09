82c9dafdb3fee503d03b3088f9faa2360cc4a487
Amro Diab <adiab@hotmail.co.uk>
2021-01-09 23:45:32 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup script for binance module
 from setuptools import setup
 
 setup(name='binance',
-      version='0.7',
+      version='0.8',
       py_modules=['binance'],
       description='Binance API wrapper',
       url='https://github.com/toshima/binance',
